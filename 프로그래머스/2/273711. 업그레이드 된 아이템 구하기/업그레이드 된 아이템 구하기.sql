ca0c1f133e2d403f75999210cc31f8c76511dae1
osh-lang <85592632+osh-lang@users.noreply.github.com>
2024-02-21 21:41:20 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 
-select e.item_id, e.item_name, e.rarity
-from item_info i, item_info e, item_tree t
-where i.rarity='RARE' and i.item_id=t.parent_item_id and t.item_id=e.item_id
-order by e.item_id desc
+SELECT E.ITEM_ID, E.ITEM_NAME, E.RARITY
+FROM ITEM_INFO I, ITEM_INFO E, ITEM_TREE T
+WHERE I.RARITY = 'RARE' AND I.ITEM_ID = T.PARENT_ITEM_ID AND T.ITEM_ID=E.ITEM_ID
+ORDER BY E.ITEM_ID DESC
